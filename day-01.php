48d21e8cee8093eade080c4a956bcef1839d6c14
Thiago Santos <thdnunes@gmail.com>
2018-03-27 23:58:15 -0700

    First 3 lessons added.

---
+++ day-01.php
@@ -0,0 +1,43 @@
+<?php 
+    $pgName = 'Lessons - Day 01';
+    include '../templates/header.php';
+?>
+        <div>
+	        <h1>Day 01</h1>
+
+	        <nav class="day-nav">
+	            <ul>
+	                <li><a href="#lorem">Lorem</a></li>
+	                <li><a href="#nihil">Nihil</a></li>
+	                <li><a href="#quibusdam">Quibusdam</a></li>
+	                <li><a href="#voluptatum">Voluptatum</a></li>
+	                <li><a href="#aperiam">Aperiam</a></li>
+	            </ul>
+	        </nav>
+        </div>
+
+	    <section class="lesson-notes">
+
+	        <article class="lesson-article" id="lorem">
+	            <h1>Lorem</h1>
+	            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti fugit laudantium a illum illo eveniet ipsam repellendus natus, sed quod nihil dignissimos temporibus ducimus eos libero omnis officia. Tenetur, molestias?</p>
+	        </article>
+	        <article class="lesson-article" id="nihil">
+	            <h1>Nihil</h1>
+	            <p>Facere sit, at similique adipisci facilis illo iure blanditiis ad. Officiis doloribus totam aliquid! Repudiandae in sapiente sunt, iusto omnis distinctio sequi fugiat nam quam consequatur velit eius, inventore quae.</p>
+	        </article>
+	        <article class="lesson-article" id="quibusdam">
+	            <h1>Quibusdam</h1>
+	            <p>Numquam nostrum atque, ducimus dicta et quas hic optio cumque odio cum voluptatum alias fugit, aliquid aut adipisci consectetur cupiditate tempore incidunt error distinctio dolore nisi earum! Aliquam, nobis enim.</p>
+	        </article>
+	        <article class="lesson-article" id="voluptatum">
+	            <h1>Voluptatum</h1>
+	            <p>Aliquam animi obcaecati cum necessitatibus perspiciatis in natus dolore doloribus quasi, est eius sed maxime eveniet esse maiores aut inventore excepturi sequi autem veritatis sapiente nulla ipsum dolor! Assumenda, voluptates.</p>
+	        </article>
+	        <article class="lesson-article" id="aperiam">
+	            <h1>Aperiam</h1>
+	            <p>Maxime incidunt voluptatem vitae hic delectus odit minima voluptatibus iusto nostrum doloribus necessitatibus accusantium, expedita error non quisquam exercitationem corporis nisi nam nulla veniam, dignissimos optio iure, adipisci at. Amet!</p>
+	        </article>
+
+	    </section>
+<?php include '../templates/footer.php'; ?>
