1d209240634c8ce7e0274554465084f7e6bf4306
Thiago Santos <thdnunes@gmail.com>
2018-04-03 16:01:01 -0700

    updated layout

--- outline.php
+++ outline.php
@@ -1,54 +1,59 @@
 <?php 
     $pgName = 'Outline';
+    $mainClass = 'main-outline';
     include './templates/header.php';
 ?>
             <div class="section-04">Course Outline</div>
 
-			<div class="section-05">Prerequisites
-			COMP 1850 or equivalent HTML5 and CSS3 knowledge.
+			<div class="section-05">
+                <h3>Prerequisites</h3>
+                <p>COMP 1850 or equivalent HTML5 and CSS3 knowledge.</p>
             </div>
 
-			<div class="section-06">Start Dates
-			Spring/Summer 2018:	Apr 10, Jun 04,	Apr 12
+			<div class="section-06">
+                <h3>Start Dates</h3>
+                <p>Spring/Summer 2018:	Apr 10, Jun 04,	Apr 12</p>
             </div>
             
             <div class="section-07">
-			Length
-			12 x 3 hr classes (36 hours total)
+                <h3>Duration</h3>
+                <p>12 x 3 hr classes (36 hours total)</p>
             </div>
             
             <div class="section-08">
-			Course Description
-			This hands-on course follows on from COMP 1850 Applied Web Development Level 1. Students who already understand HTML and CSS will explore web development in more depth with new topics and techniques. Participants will gain a deeper understanding of the latest technologies including HTML5 and CSS3 for developing web sites and applications. There is a focus on responsive design and mobile presentation through the use of Media Queries. Adding behavior to web sites using JavaScript frameworks such as jQuery is introduced. Labs and exercises focus on industry standard web development methodology. Students gain experience creating templates for content management systems and presentation. Additional topics will include: source control for managing your code, and web-based fonts for professional typography.
-			By the end of this course successful students will be able to build dynamic and semantically marked-up content that is viewable on mobile and desktop screen sizes. 
+                <h3>Course Description</h3>
+			<p>This hands-on course follows on from COMP 1850 Applied Web Development Level 1. Students who already understand HTML and CSS will explore web development in more depth with new topics and techniques. Participants will gain a deeper understanding of the latest technologies including HTML5 and CSS3 for developing web sites and applications. 
+                <p>There is a focus on responsive design and mobile presentation through the use of Media Queries. Adding behavior to web sites using JavaScript frameworks such as jQuery is introduced. </p>
+                <p>Labs and exercises focus on industry standard web development methodology. Students gain experience creating templates for content management systems and presentation. Additional topics will include: source control for managing your code, and web-based fonts for professional typography.</p>
+                <p>By the end of this course successful students will be able to build dynamic and semantically marked-up content that is viewable on mobile and desktop screen sizes. </p>
             </div>
 
             <div class="section-09">
-			Course Goals
-			•	To gain a deeper understanding of HTML, CSS and advanced topics in web development.
-			•	To focus on best practices in web page and web site design.
-			Course Learning Outcomes / Competencies:
-			Upon successful completion of this course, the student will be able to:
-			•	To gain a deeper understanding of HTML, CSS and advanced topics in web development.
-			•	To focus on best practices in web page and web site design.
-			•	Add behaviour to any web page using JavaScript.
-			•	Create dynamic content using JavaScript frameworks like jQuery.
-			•	Employ Web Standards when building web sites for maximum compatibility.
-			•	Understand basic development methodology.
-			•	Use media queries to create responsive designs for mobile and desktop presentation
-			•	Use HTML5 technologies to present rich content such as video and audio.
-			•	Create professional looking typography using web-based fonts.
-			•	Understand source control using GIT.
+                <h3>Course Goals</h3>
+                <ol>
+                    <li>To gain a deeper understanding of HTML, CSS, and advanced topics in web development.</li>
+			<li>To focus on best practices in web page and web site design.</li>
+			<li>To gain a deeper understanding of HTML, CSS and advanced topics in web development.</li>
+			<li>To focus on best practices in web page and web site design.</li>
+			<li>Add behaviour to any web page using JavaScript.</li>
+			<li>Create dynamic content using JavaScript frameworks like jQuery.</li>
+			<li>Employ Web Standards when building web sites for maximum compatibility.</li>
+			<li>Understand basic development methodology.</li>
+			<li>Use media queries to create responsive designs for mobile and desktop presentation</li>
+			<li>Use HTML5 technologies to present rich content such as video and audio.</li>
+			<li>Create professional looking typography using web-based fonts.</li>
+			<li>Understand source control using GIT.</li>
+                </ol>
             </div>
 			 
             <div class="section-10">
-			Evaluation Criteria
-			Assignments 1 – 7	20% (2.86% per assignment)
-			Project 1		20%
-			Mid-Term Exam		10%
-			Final Exam		20%
-			Final Project		30%
-			Total			100%
+                <h3>Evaluation Criteria</h3>
+                <p>Assignments 1 – 7	20% (2.86% per assignment)</p>
+                <p>Project 1		20%</p>
+                <p>Mid-Term Exam		10%</p>
+                <p>Final Exam		20%</p>
+                <p>Final Project		30%</p>
+                <p>Total			100%</p>
             </div>
 
 <?php include './templates/footer.php'; ?>
