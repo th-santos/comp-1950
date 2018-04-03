50698bc34096c9b9f5b1d7824fc298a89bd11b4b
Gergely S <mightymallow14@gmail.com>
2018-04-02 17:08:52 -0700

    completed outline and schedule html pages

--- outline.php
+++ outline.php
@@ -2,22 +2,28 @@
     $pgName = 'Outline';
     include './templates/header.php';
 ?>
-        <div>
-	        <h1>Course Outline</h1>
+            <div class="section-04">Course Outline</div>
 
-			Prerequisites
+			<div class="section-05">Prerequisites
 			COMP 1850 or equivalent HTML5 and CSS3 knowledge.
+            </div>
 
-			Start Dates
+			<div class="section-06">Start Dates
 			Spring/Summer 2018:	Apr 10, Jun 04,	Apr 12
-
+            </div>
+            
+            <div class="section-07">
 			Length
 			12 x 3 hr classes (36 hours total)
-
+            </div>
+            
+            <div class="section-08">
 			Course Description
 			This hands-on course follows on from COMP 1850 Applied Web Development Level 1. Students who already understand HTML and CSS will explore web development in more depth with new topics and techniques. Participants will gain a deeper understanding of the latest technologies including HTML5 and CSS3 for developing web sites and applications. There is a focus on responsive design and mobile presentation through the use of Media Queries. Adding behavior to web sites using JavaScript frameworks such as jQuery is introduced. Labs and exercises focus on industry standard web development methodology. Students gain experience creating templates for content management systems and presentation. Additional topics will include: source control for managing your code, and web-based fonts for professional typography.
 			By the end of this course successful students will be able to build dynamic and semantically marked-up content that is viewable on mobile and desktop screen sizes. 
+            </div>
 
+            <div class="section-09">
 			Course Goals
 			•	To gain a deeper understanding of HTML, CSS and advanced topics in web development.
 			•	To focus on best practices in web page and web site design.
@@ -33,7 +39,9 @@
 			•	Use HTML5 technologies to present rich content such as video and audio.
 			•	Create professional looking typography using web-based fonts.
 			•	Understand source control using GIT.
+            </div>
 			 
+            <div class="section-10">
 			Evaluation Criteria
 			Assignments 1 – 7	20% (2.86% per assignment)
 			Project 1		20%
@@ -41,5 +49,6 @@
 			Final Exam		20%
 			Final Project		30%
 			Total			100%
-        </div>
+            </div>
+
 <?php include './templates/footer.php'; ?>
--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,4 @@
     </main>
-    <hr style="border-color: darkturquoise;">
     <footer>
         <p>Created by Gergely and Thiago for COMP 1950 Spring 2018!</p>
     </footer>
