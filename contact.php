cf119cb4a87af4080b91921dc541af4f9534185d
Thiago Santos <thdnunes@gmail.com>
2018-04-03 20:36:50 -0700

    add col-1 class

--- contact.php
+++ contact.php
@@ -2,48 +2,49 @@
     $pgName = 'Contact';
     include './templates/header.php';
 ?>
+	<main class="col-1">
         <div>
 	        <h1>Contact Us</h1>
 
 	        <form action="/thank-you.php" method="post" class="contact">
 	        
-	            <div class="form-group fn">
+	            <div>
 	                <label for="fn">Firstname: </label>
 	                <input type="text" id="fn" name="fn" required>
 	            </div>
 
-	            <div class="form-group ln">
+	            <div>
 	                <label for="fn">lastname: </label>
 	                <input type="text" id="ln" name="ln" required>
 	            </div>
 
-	            <div class="form-group em">
+	            <div>
 	                <label for="em">Email: </label>
 	                <input type="text" id="em" name="em" required>
 	            </div>
 
-	            <div class="form-group st">
-	                <div class="radio-group">
+	            <div>
+	                <div>
 	                    <input type="radio" id="ps" name="st" value="ps">
 	                    <label for="ps" class="rd-label">Prospective Student</label>
 	                </div>
-	                <div class="radio-group">
+	                <div>
 	                    <input type="radio" id="cs" name="st" value="cs">
 	                    <label for="cs" class="rd-label">Current Student</label>
 	                </div>            
 	            </div>
 
-	            <div class="form-group sn">
+	            <div>
 	                <label for="sn">BCIT Student Number: </label>
 	                <input type="text" id="sn" name="sn" required pattern="[A-Za-z](\d{8})" title="Format: A12345678">
 	            </div>
 
-	            <div class="form-group ct">
+	            <div>
 	                <label for="ct">Message: </label>
 	                <textarea name="ct" id="ct"></textarea>
 	            </div>
 	        
-	            <div class="form-group sb">
+	            <div>
 	                <input type="submit" id="sb" name="sb" value="Submit">
 	            </div>
 	        
