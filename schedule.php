1c48ad624da6ea1900281bea4eef11790ebad87a
Thiago Santos <thdnunes@gmail.com>
2018-04-03 17:06:52 -0700

    layout updated - same as html version

--- schedule.php
+++ schedule.php
@@ -2,56 +2,114 @@
     $pgName = 'Schedule';
     include './templates/header.php';
 ?>
-        <div>
-	        <h1>Schedule</h1>
+    <div class="section-11">Schedule</div>
+        
+    <main class="main-schedule">
 	        
-			Day 01
-			-	Course Introduction
-			-	Setup development environment
-			-	FTP Setup
-			-	HTML and CSS Review
-			-	HTML5 Video and Audio
-			-	Assignment 1
-			Day 02
-			-	CSS @font-face
-			-	Responsive web design
-			-	Assignment 2
-			Day 03
-			-	Flexbox
-			-	Assignment 3
-			Day 04
-			-	CSS3 styling
-			-	CSS3 selectors
-			-	CSS transitions and animations
-			-	Assignment 4
-			 
-			Day 05
-			-	Responsive Images
-			-	Mobile navigation systems
-			-	Assignment 5
-			-	Mid-term project
-			Day 06
-			-	Mid Term Exam
-			-	Lab time
-			Day 07
-			-	CSS Grid
-			-	Assignment 6
-			Day 08
-			-	SASS
-			-	Assignment 7
-			Day 09
-			-	Git and GitHub
-			-	Final Group Project
-			Day 10
-			-	Servers
-			-	htaccess
-			-	PHP review
-			-	JavaScript review
-			Day 11
-			-	Lab time
-			Day 12
-			-	Final Exam
-			-	Lab time
-
+        <div class="section-12">
+			<h3>Day 01</h3>
+            <ul>
+                <li>Course Introduction</li>
+                <li>Setup development environment</li>
+                <li>FTP Setup</li>
+                <li>HTML and CSS Review</li>
+                <li>HTML5 Video and Audio</li>
+                <li>Assignment 1</li>
+            </ul>
         </div>
+        
+        <div class="section-13">
+			<h3>Day 02</h3>
+            <ul>
+                <li>CSS @font-face</li>
+                <li>Responsive web design</li>
+                <li>Assignment 2</li>
+            </ul>
+        </div>
+            
+        <div class="section-14">    
+            <h3>Day 03</h3>
+            <ul>
+                <li>Flexbox</li>
+                <li>Assignment 3</li>
+            </ul>
+            </div>
+            
+        <div class="section-15">    
+			<h3>Day 04</h3>
+            <ul>
+            <li>CSS3 styling</li>
+            <li>CSS3 selectors</li>
+            <li>CSS transitions and animations</li>
+            <li>Assignment 4</li>
+            </ul>
+            </div>
+			 
+        <div class="section-16">    
+			<h3>Day 05</h3>
+            <ul>
+			<li>Responsive Images</li>
+			<li>Mobile navigation systems</li>
+			<li>Assignment 5</li>
+			<li>Mid-term project</li>
+            </ul>
+            </div>
+            
+        <div class="section-17">    
+			<h3>Day 06</h3>
+			<ul>
+            <li>Mid Term Exam</li>
+			<li>Lab time</li>
+            </ul>
+            </div>
+            
+        <div class="section-18">    
+			<h3>Day 07</h3>
+			<ul>
+            <li>CSS Grid</li>
+			<li>Assignment 6</li>
+            </ul>
+            </div>
+            
+        <div class="section-19">    
+			<h3>Day 08</h3>
+            <ul>
+			<li>SASS</li>
+			<li>Assignment 7</li>
+            </ul>
+            </div>
+            
+        <div class="section-20">    
+			<h3>Day 09</h3>
+            <ul>
+			<li>Git and GitHub</li>
+			<li>Final Group Project</li>
+            </ul>
+            </div>
+            
+        <div class="section-21">    
+			<h3>Day 10</h3>
+            <ul>
+            <li>Servers</li>
+			<li>htaccess</li>
+			<li>PHP review</li>
+			<li>JavaScript review</li>
+            </ul>
+            </div>
+            
+        <div class="section-22">    
+			<h3>Day 11</h3>
+            <ul>
+        <li>Lab time</li>
+            </ul>
+            </div>
+            
+        <div class="section-23">    
+			<h3>Day 12</h3>
+            <ul>
+            <li>Final Exam</li>
+            <li>Lab time</li>
+            </ul>
+            </div>
+        
 <?php include './templates/footer.php'; ?>
